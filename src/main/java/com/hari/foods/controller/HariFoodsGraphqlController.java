b60369b0a29faf1d560585d04c04c6ec1e8913d1
raviteja992 <ravitejatech99@gmail.com>
2023-11-17 22:06:56 +0530

    Added Graphql Controller

--- src/main/java/com/hari/foods/controller/HariFoodsGraphqlController.java
+++ src/main/java/com/hari/foods/controller/HariFoodsGraphqlController.java
@@ -1,15 +1,28 @@
 package com.hari.foods.controller;
 
+import com.hari.foods.model.Item;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HariFoodsGraphqlController {
 
     @QueryMapping
-    public List<String> items(){
-        return null;
+    public List<Item> items(){
+        Item item = new Item();
+        item.setName("Chicken");
+        item.setPrice(300f);
+        item.setType("Nonveg");
+        Item item2 = new Item();
+        item2.setName("Mutton");
+        item2.setPrice(300f);
+        item2.setType("Nonveg");
+        List<Item> items = new ArrayList<>();
+        items.add(item);
+        items.add(item2);
+        return items;
     }
 }
---
+++ src/main/java/com/hari/foods/model/Item.java
@@ -0,0 +1,11 @@
+package com.hari.foods.model;
+
+import lombok.Data;
+
+@Data
+public class Item {
+
+    private String name;
+    private Float price;
+    private String type;
+}
