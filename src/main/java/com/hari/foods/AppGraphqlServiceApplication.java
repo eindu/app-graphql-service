2d8418d8fa3693e27e808e3eb84eb8d59e67b9b5
raviteja992 <ravitejatech99@gmail.com>
2023-11-17 16:39:22 +0530

    Initial Commit

---
+++ src/main/java/com/hari/foods/AppGraphqlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hari.foods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGraphqlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGraphqlServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hari/foods/AppGraphqlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hari.foods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGraphqlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
